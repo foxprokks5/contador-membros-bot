bced53aedc04d586c279a60e92ca95cad4cd875c
foxprokks5 <robloxmeu100@gmail.com>
2026-07-04 18:39:00 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online! ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
